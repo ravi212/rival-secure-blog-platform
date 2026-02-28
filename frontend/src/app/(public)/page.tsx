7aac07d86fd17f56a6c4291e0b68445e2196600d
ravi212 <raviraina212@gmail.com>
2026-02-28 20:52:35 +0530

    FE: Build Fix

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,11 +1,9 @@
-import FeaturedBlogs from "@/components/landing/FeaturedBlog";
 import HeroSection from "@/components/landing/HeroSection";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
-      <FeaturedBlogs />
     </>
   );
 }
